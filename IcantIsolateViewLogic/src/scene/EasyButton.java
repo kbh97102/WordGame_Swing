c39ef19b0cb568e16310aa06df26f23857b2d332
Arakene <kbh97102@naver.com>
2019-11-20 18:28:31 +0900

    LevelSelect UI Upgrade

---
+++ IcantIsolateViewLogic/src/scene/EasyButton.java
@@ -0,0 +1,65 @@
+package scene;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class EasyButton {
+
+    private JPanel buttonContentPanel;
+    private JButton difficultyButton;
+    private JLabel blindOption;
+    private JLabel speedOption;
+    private JLabel bestScore;
+    private JLabel healWordOption;
+
+
+    private String difficulty;
+    private String existBlind;
+    private int speed;
+    private int highScore;
+    private String existHeal;
+
+
+    public EasyButton(String difficulty, String existBlind, String existHeal, int speed, int highScore){
+        init();
+        this.difficulty = difficulty;
+        this.existBlind = existBlind;
+        this.existHeal = existHeal;
+        this.speed = speed;
+        this.highScore = highScore;
+
+        difficultyButton.setText(difficulty);
+        blindOption.setText(existBlind);
+        healWordOption.setText(existHeal);
+        speedOption.setText(Integer.toString(speed));
+        bestScore.setText(Integer.toString(highScore));
+
+
+        buttonContentPanel.setLayout(new GridLayout(9,1,0,5));
+        buttonContentPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK ,1));
+        buttonContentPanel.add(difficultyButton);
+        buttonContentPanel.add(blindOption);
+        buttonContentPanel.add(speedOption);
+        buttonContentPanel.add(healWordOption);
+        buttonContentPanel.add(bestScore);
+        buttonContentPanel.add(new JPanel());
+        buttonContentPanel.add(new JPanel());
+        buttonContentPanel.add(new JPanel());
+        buttonContentPanel.add(new JPanel());
+
+        buttonContentPanel.setBackground(Color.BLACK);
+
+    }
+
+    public void init(){
+        buttonContentPanel = new JPanel();
+        difficultyButton = new JButton();
+        blindOption = new JLabel();
+        speedOption = new JLabel();
+        healWordOption = new JLabel();
+        bestScore = new JLabel();
+    }
+    public JPanel getEasyPanel(){
+        return buttonContentPanel;
+    }
+}
--- IcantIsolateViewLogic/src/scene/LevelSelect.java
+++ IcantIsolateViewLogic/src/scene/LevelSelect.java
@@ -7,39 +7,51 @@ public class LevelSelect extends PanelArray{
 
     private  LevelSelect levelSelectInstance;
     private JButton button;
-    private JButton easyButton;
+    private EasyButton easyButton;
     private JButton normalButton;
     private JButton hardButton;
 
+    private JPanel buttonPanel;
+
+    private EasyButton t2;
+    private EasyButton t3;
+
+    private JLabel levelTitle;
     private JPanel levelPanel;
     public LevelSelect(){
         init();
+        t2 = new EasyButton("normal", "true", "false", 2,100);
+        t3 = new EasyButton("hard", "true", "true", 3, 50);
 
-        levelPanel.setLayout(new GridLayout(7,1));
-        contentPanel.setLayout(new BorderLayout());
 
-        levelPanel.add(easyButton);
-        levelPanel.add(new JLabel());
-        levelPanel.add(normalButton);
-        levelPanel.add(new JLabel());
-        levelPanel.add(hardButton);
-        levelPanel.add(new JLabel());
-        levelPanel.add(button);
+        contentPanel.setLayout(new BorderLayout(0,50));
+        buttonPanel.setLayout(new GridLayout(1,5,100,0));
 
-        contentPanel.add(levelPanel, BorderLayout.CENTER);
-        contentPanel.setBackground(Color.ORANGE);
+        //
+        levelTitle.setIcon(new ImageIcon("./src/Image/leveltitleImage.png"));
+        levelTitle.setHorizontalAlignment(JLabel.CENTER);
+        //Girdlayout gap
+        buttonPanel.add(new JPanel());
+        buttonPanel.add(easyButton.getEasyPanel());
+        buttonPanel.add(t2.getEasyPanel());
+        buttonPanel.add(t3.getEasyPanel());
+        buttonPanel.add(new JPanel());
 
+        contentPanel.add(levelTitle, BorderLayout.NORTH);
+        contentPanel.add(buttonPanel, BorderLayout.CENTER);
         //event
-        easyButton.addActionListener(event -> sceneChange.accept(Scene.INGAME));
+//        easyButton.addActionListener(event -> sceneChange.accept(Scene.INGAME));
         button.addActionListener(event-> sceneChange.accept(Scene.MAIN));
     }
     private void init(){
         levelPanel = new JPanel();
         contentPanel = new JPanel();
         button = new JButton("click");
-        easyButton = new JButton("easy");
+        easyButton = new EasyButton("easy", "false", "true", 1, 300);
         normalButton = new JButton("normal");
         hardButton = new JButton("hard");
+        buttonPanel = new JPanel();
+        levelTitle = new JLabel();
     }
 
 }
